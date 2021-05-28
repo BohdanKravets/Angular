d807b1a4dab50dae7907b22a96c72d495da31e52
BohdanKravets <bodyakravets@gmail.com>
2021-05-28 14:27:17 +0300

    hw1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//Взяти інформіцію з https://jsonplaceholder.typicode.com/posts і вивести її
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
