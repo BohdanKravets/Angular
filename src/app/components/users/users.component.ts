34c58e6f5421f3f9b9aa15a0a9b0f0a799151a83
BohdanKravets <bodyakravets@gmail.com>
2021-05-31 12:37:21 +0300

    random

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:<User[]>) { }
 
   ngOnInit(): void {
 
