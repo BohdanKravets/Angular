cac5aef7065ee69e55635b1eedaa956c5a59e6be
BohdanKravets <bodyakravets@gmail.com>
2021-05-31 12:32:57 +0300

    random

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { UsersComponent } from './components/users/users.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,HttpClientModule
