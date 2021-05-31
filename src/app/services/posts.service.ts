aa03bc7a427513bdf13b42ccf926aa412e5e369f
BohdanKravets <bodyakravets@gmail.com>
2021-06-01 00:15:14 +0300

    practice1

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IPost} from "../interfaces";
+import {urls} from "../constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getAll(): Observable<IPost[]> {
+    return this.httpClient.get<IPost[]>(urls.posts);
+  }
 }
