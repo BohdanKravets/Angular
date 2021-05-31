5d4e584f06e8340cd1c721eac13fcc77af7cdbca
BohdanKravets <bodyakravets@gmail.com>
2021-05-31 22:54:34 +0300

    random

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentsComponent } from './components/comments/comments.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,8 @@
 export const environment = {
   production: false
 };
+export const URL = 'https://jsonplaceholder.typicode.com/'
+
 
 /*
  * For easier debugging in development mode, you can import the following file
