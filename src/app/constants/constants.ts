7e3f948a75ca84f5ccfa6fdf464d6f8f0b5f2017
BohdanKravets <bodyakravets@gmail.com>
2021-06-01 14:36:47 +0300

    practice1

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
-import {API} from '../../environments/environment'
+import {environment} from '../../environments/environment'
 
-
+const {API} = environment;
 export const urls = {
   comments: `${API}/comments`,
   posts: `${API}/posts`
