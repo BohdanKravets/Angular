348817d53adefcaf7735c01e1eb0e3f528dff47c
BohdanKravets <bodyakravets@gmail.com>
2021-06-08 18:40:07 +0300

    practice4

---
+++ src/app/services/resolvers/posts-resolver.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {IPost} from "../../interfaces";
+import {Observable} from "rxjs";
+import {PostsService} from "../posts.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsResolverService implements Resolve<IPost[]>{
+
+  constructor(private postsService:PostsService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IPost[]> | Promise<IPost[]> | IPost[] {
+    return this.postsService.getAll();
+  }
+}
