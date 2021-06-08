de9da145e60c5231612d831f4e0d141d6983d3e5
BohdanKravets <bodyakravets@gmail.com>
2021-06-08 19:10:03 +0300

    practice4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 
 
+//поділити на два модулі users и posts
+// при кліку на кнопку юзера показати його пости, при кліку на кнопку поста показати його коментарі
 @NgModule({
   declarations: [
     AppComponent,
